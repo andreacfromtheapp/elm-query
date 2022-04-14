317b9c04c40c4230968fa4df60817db1f1115199
gacallea <gacallea@users.noreply.github.com>
2022-04-14 14:50:29 +0200

    added try catch and error message

--- elm_query.py
+++ elm_query.py
@@ -15,17 +15,35 @@ class ElmPackageCommandCommand(sublime_plugin.WindowCommand):
 
 def elm_package(input):
     def on_done(index):
-        if index > 0:
+        if index >= 0:
             webbrowser.open(homepage, 2)
 
     url = "https://package.elm-lang.org/search.json"
-    text = urllib.request.urlopen(url).read().decode()
-    data = json.loads(text)
-    results = [x for x in data if input in x["name"] or input in x["summary"]]
-
+    error = False
+    results = []
     package_list = []
 
-    if results:
+    try:
+        text = urllib.request.urlopen(url).read().decode()
+        data = json.loads(text)
+        results = [
+            x for x in data if input in x["name"] or input in x["summary"]
+        ]
+    except IOError:
+        error = True
+
+    if error:
+        message = "Something did not go as intended... Sorry."
+        summary = "Press <Enter> to file an issue on GitHub"
+        description = "<em>%s</em>" % sublime.html_format_command(summary)
+        homepage = "https://github.com/gacallea/elm-query/issues"
+        final_line = '<a href="{}">{}</a>'.format(homepage, homepage)
+        error_entry = sublime.QuickPanelItem(
+            message, [description, final_line]
+        )
+        package_list.append(error_entry)
+
+    elif results:
         for x in results:
             package = x["name"]
             summary = x["summary"]
@@ -43,24 +61,13 @@ def elm_package(input):
                 package, [description, final_line]
             )
             package_list.append(package_entry)
+
     else:
         package_list.append(
             "No results found for the keyword '{}'".format(input)
         )
 
     sublime.active_window().show_quick_panel(package_list, on_done)
-
-
-# this would be nice, but the elm packages site doesn't work that way....
-# class ElmPackageSelectionCommand(sublime_plugin.TextCommand):
-#     def run(self, _):
-#         # query
-#         selection = self.view.sel()[0]
-#         if len(selection) == 0:
-#             selection = self.view.word(selection)
-#         query = self.view.substr(selection)
-
-#         elm_package(query)
 
 
 class ElmSearchCommandCommand(sublime_plugin.WindowCommand):
