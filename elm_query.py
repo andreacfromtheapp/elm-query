36bc6b75eded46654b0404df5558707e473ac47d
gacallea <gacallea@users.noreply.github.com>
2022-04-14 16:44:59 +0200

    mv function to method + attributes. better for future implementations

--- elm_query.py
+++ elm_query.py
@@ -6,68 +6,69 @@ import urllib.parse
 import json
 
 
-class ElmPackageCommandCommand(sublime_plugin.WindowCommand):
+class ElmPackageCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.window.show_input_panel(
-            "Elm Packages", "", elm_package, None, None
+            "Elm Packages", "", self.package_search, None, None
         )
 
+    def package_search(self, input):
+        self.url = "https://package.elm-lang.org/search.json"
+        self.results = []
+        self.package_list = []
+        self.error = False
 
-def elm_package(input):
-    def on_done(index):
-        if index >= 0:
-            webbrowser.open(homepage, 2)
+        def on_done(index):
+            if index >= 0:
+                webbrowser.open(homepage, 2)
 
-    url = "https://package.elm-lang.org/search.json"
-    error = False
-    results = []
-    package_list = []
+        try:
+            text = urllib.request.urlopen(self.url).read().decode()
+            data = json.loads(text)
+            self.results = [
+                x for x in data if input in x["name"] or input in x["summary"]
+            ]
+        except IOError:
+            self.error = True
 
-    try:
-        text = urllib.request.urlopen(url).read().decode()
-        data = json.loads(text)
-        results = [
-            x for x in data if input in x["name"] or input in x["summary"]
-        ]
-    except IOError:
-        error = True
-
-    if error:
-        message = "Something did not go as intended... Sorry."
-        summary = "Press <Enter> to file an issue on GitHub"
-        description = "<em>%s</em>" % sublime.html_format_command(summary)
-        homepage = "https://github.com/gacallea/elm-query/issues"
-        final_line = '<a href="{}">{}</a>'.format(homepage, homepage)
-        error_entry = sublime.QuickPanelItem(
-            message, [description, final_line]
-        )
-        package_list.append(error_entry)
-
-    elif results:
-        for x in results:
-            package = x["name"]
-            summary = x["summary"]
-            version = x["version"]
+        if self.error:
+            message = "Something did not go as intended... Sorry."
+            summary = "Press <Enter> to file an issue on GitHub"
             description = "<em>%s</em>" % sublime.html_format_command(summary)
-            homepage = (
-                "https://package.elm-lang.org/packages/{}/latest/".format(
-                    package
+            homepage = "https://github.com/gacallea/elm-query/issues"
+            final_line = '<a href="{}">{}</a>'.format(homepage, homepage)
+            error_entry = sublime.QuickPanelItem(
+                message, [description, final_line]
+            )
+            self.package_list.append(error_entry)
+
+        elif self.results:
+            for x in self.results:
+                package = x["name"]
+                summary = x["summary"]
+                version = x["version"]
+                description = "<em>%s</em>" % sublime.html_format_command(
+                    summary
                 )
-            )
-            final_line = 'version: {}; <a href="{}">{}</a>'.format(
-                version, homepage, homepage
-            )
-            package_entry = sublime.QuickPanelItem(
-                package, [description, final_line]
-            )
-            package_list.append(package_entry)
+                homepage = (
+                    "https://package.elm-lang.org/packages/{}/latest/".format(
+                        package
+                    )
+                )
+                final_line = 'version: {}; <a href="{}">{}</a>'.format(
+                    version, homepage, homepage
+                )
+                package_entry = sublime.QuickPanelItem(
+                    package, [description, final_line]
+                )
+                self.package_list.append(package_entry)
 
-    else:
-        package_list.append(
-            "No results found for the keyword '{}'".format(input)
-        )
+        else:
+            self.package_list.append(
+                "No results found for the keyword '{}'".format(input)
+            )
 
-    sublime.active_window().show_quick_panel(package_list, on_done)
+        sublime.active_window().show_quick_panel(self.package_list, on_done)
 
 
 class ElmSearchCommandCommand(sublime_plugin.WindowCommand):
