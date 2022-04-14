455f33095247d2496a3f216b3431e0fe16bbfac5
gacallea <gacallea@users.noreply.github.com>
2022-04-14 12:33:28 +0200

    1st implementation of elm packages search

--- elm_query.py
+++ elm_query.py
@@ -1,55 +1,9 @@
-# Elm Search plugin for Sublime Text
-
 import sublime
 import sublime_plugin
 import webbrowser
 import urllib.request
 import urllib.parse
 import json
-import re
-from html.parser import HTMLParser
-
-
-# Global varz
-results = []
-
-
-class ElmSearchCommandCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.show_input_panel("Elm Search", "", elm_search, None, None)
-
-
-class ElmSearchSelectionCommand(sublime_plugin.TextCommand):
-    def run(self, _):
-
-        # query
-        selection = self.view.sel()[0]
-        if len(selection) == 0:
-            selection = self.view.word(selection)
-        query = self.view.substr(selection)
-
-        elm_search(query)
-
-
-def elm_search(input):
-    global results
-
-    query = urllib.parse.quote_plus(input)
-    url = "https://klaftertief.github.io/elm-search/?q=" + query
-    data = urllib.request.urlopen(url).read().decode()
-    results = json.loads(data)
-
-    formatedResult = []
-
-    if results:
-        for result in results:
-            formatted = format(result)
-            if formatted != "":
-                formatedResult.append(formatted)
-    else:
-        formatedResult.append("No results")
-
-    sublime.active_window().show_quick_panel(formatedResult, on_done)
 
 
 class ElmPackageCommandCommand(sublime_plugin.WindowCommand):
@@ -59,7 +13,6 @@ class ElmPackageCommandCommand(sublime_plugin.WindowCommand):
 
 class ElmPackageSelectionCommand(sublime_plugin.TextCommand):
     def run(self, _):
-
         # query
         selection = self.view.sel()[0]
         if len(selection) == 0:
@@ -70,48 +23,58 @@ class ElmPackageSelectionCommand(sublime_plugin.TextCommand):
 
 
 def elm_package(input):
-    global results
+    def on_done(index):
+        if index > 0:
+            webbrowser.open(homepage, 2)
 
-    query = urllib.parse.quote_plus(input)
-    url = "https://package.elm-lang.org/" + query
-    data = urllib.request.urlopen(url).read().decode()
-    results = json.loads(data)
+    url = "https://package.elm-lang.org/search.json"
+    text = urllib.request.urlopen(url).read().decode()
+    data = json.loads(text)
+    results = [x for x in data if input in x["name"] or input in x["summary"]]
 
-    formatedResult = []
+    package_list = []
 
     if results:
-        for result in results:
-            formatted = format(result)
-            if formatted != "":
-                formatedResult.append(formatted)
+        for x in results:
+            package = x["name"]
+            summary = x["summary"]
+            version = x["version"]
+            description = "<em>%s</em>" % sublime.html_format_command(summary)
+            homepage = "https://package.elm-lang.org/packages/{}/latest/".format(
+                package
+            )
+            final_line = 'version: {}; <a href="{}">{}</a>'.format(
+                version, homepage, homepage
+            )
+            package_entry = sublime.QuickPanelItem(package, [description, final_line])
+            package_list.append(package_entry)
     else:
-        formatedResult.append("No results")
+        package_list.append("No results")
 
-    sublime.active_window().show_quick_panel(formatedResult, on_done)
-
-
-def on_done(index):
-    if index != -1:
-        webbrowser.open(results[index]["url"], 2)
+    sublime.active_window().show_quick_panel(package_list, on_done)
 
 
-def format(result):
-    if result.get("module") is None:  # Get module name if it exists
-        return ""
-    else:
-        modname = result.get("module").get("name")
-        if modname is None:
-            return ""
+# class ElmSearchCommandCommand(sublime_plugin.WindowCommand):
+#     def run(self):
+#         self.window.show_input_panel("Elm Search", "", elm_search, None, None)
 
-    if result.get("item") is None:  # Get type signature
-        return ""
-    else:
-        # split type siganture into name and type
-        res = result["item"].split(":")
-        name = re.sub("<[^<]+?>", "", res[0])  # remove all html tags
-        if len(res) == 1:  # the type might not be included
-            ret = modname + " " + name
-        else:
-            ret = modname + " " + name + " " + ":" + res[1]
 
-    return HTMLParser().unescape(ret)  # decode html entities
+# class ElmSearchSelectionCommand(sublime_plugin.TextCommand):
+#     def run(self, _):
+
+#         # query
+#         selection = self.view.sel()[0]
+#         if len(selection) == 0:
+#             selection = self.view.word(selection)
+#         query = self.view.substr(selection)
+
+#         elm_search(query)
+
+
+# def elm_search(input):
+#     global results
+
+#     query = urllib.parse.quote_plus(input)
+#     url = "https://klaftertief.github.io/elm-search/?q=" + query
+#     data = urllib.request.urlopen(url).read().decode()
+#     results = json.loads(data)
